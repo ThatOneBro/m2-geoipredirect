24503ff15cd27665a874a012a048f63198b609e0
Sajid Hussain <sajidunnar@gmail.com>
2017-01-22 18:26:50 +0500

    Applying Fixes
    We must add geoip  response to session to avoid recalling for reach
    request

--- code/OneBro/GeoIpRedirect/Model/GeoIpRequest.php
+++ code/OneBro/GeoIpRedirect/Model/GeoIpRequest.php
@@ -2,54 +2,53 @@
 
 namespace OneBro\GeoIpRedirect\Model;
 
-use Magento\Framework\HTTP\Client\CurlFactory;
 use Magento\Framework\App\Request\Http;
+use Magento\Framework\HTTP\Client\CurlFactory;
 use Magento\Framework\Json\Helper\Data;
 
-class GeoIpRequest
-{
+class GeoIpRequest {
 	const REQUEST_TIMEOUT = 2000;
-	
+
 	const ENDPOINT_URL = 'http://freegeoip.net';
-	
+
 	const REQUEST_TYPE = 'json';
-	
+
 	private $response;
-	
+
 	public function __construct(
 		CurlFactory $curlFactory,
 		Http $http,
 		Data $jsonHelper
-		
+
 	) {
 		$this->curlFactory = $curlFactory;
 		$this->http = $http;
 		$this->jsonHelper = $jsonHelper;
 	}
-	
-	public function getCountryCode(){
-		$this->getGeoIpResponse()->country_code;
+
+	public function getCountryCode() {
+		return $this->getGeoIpResponse()->country_code;
 	}
-	
-	public function getGeoIpResponse(){
-		if(!$this->response){
+
+	public function getGeoIpResponse() {
+		if (!$this->response) {
 			$this->response = (object) $this->getResponseFromEndpoint();
 		}
 		return $this->response;
 	}
-	
-	private function getResponseFromEndpoint(){
+
+	private function getResponseFromEndpoint() {
 		return $this->jsonHelper->jsonDecode($this->getResponse());
 	}
-	
-	private function getResponse(){
+
+	private function getResponse() {
 		$client = $this->curlFactory->create();
-		
+
 		$client->setTimeout(self::REQUEST_TIMEOUT);
-		$client->get{
-			self::ENDPOINT_URL . '/' . self::REQUEST_TYPE . '/' . 
+		$client->get(
+			self::ENDPOINT_URL . '/' . self::REQUEST_TYPE . '/' .
 			$this->http->getClientIp()
-		};
+		);
 		return $client->getBody();
 	}
 }
--- code/OneBro/GeoIpRedirect/Plugin/GeoIpRedirectPlugin.php
+++ code/OneBro/GeoIpRedirect/Plugin/GeoIpRedirectPlugin.php
@@ -4,35 +4,52 @@ namespace OneBro\GeoIpRedirect\Plugin;
 
 use Magento\Framework\App\FrontControllerInterface;
 use Magento\Framework\App\RequestInterface;
-use Magento\Store\Model\StoreFactory;
 use Magento\Store\Api\StoreCookieManagerInterface;
+use Magento\Store\Model\StoreFactory;
+use Magento\Store\Model\StoreManagerInterface;
 use OneBro\GeoIpRedirect\Model\GeoIpRequest;
 
-class GeoIpRedirectPlugin
-{
+class GeoIpRedirectPlugin {
+
+	protected $_storeManager;
+	protected $storeFactory;
+	protected $storeCookieManager;
+	protected $geoIpRequest;
+
 	public function __construct(
 		StoreFactory $storeFactory,
 		StoreCookieManagerInterface $storeCookieManager,
-		GeoIpRequest $geoIpRequest
-	)
-	{
+		GeoIpRequest $geoIpRequest,
+		StoreManagerInterface $storeManager
+
+	) {
 		$this->storeFactory = $storeFactory;
 		$this->storeCookieManager = $storeCookieManager;
 		$this->geoIpRequest = $geoIpRequest;
+		$this->_storeManager = $storeManager;
+
 	}
-	
+
 	public function beforeDispatch(
 		FrontControllerInterface $subject,
 		RequestInterface $request
 	) {
+
+		// this request can make the website perfomrnace slow it must be stored in session 
 		$countryCode = $this->geoIpRequest->getCountryCode();
-		
-		$store = $this->storeFactory->create();
-		
-		$store->load($countryCode, 'country_code');
-		
-		$this->storeCookieManager->setStoreCookie($store);
-		
+
+		// if ($countryCode == "_choose_store_code") { // or other logic 
+			
+			// make sure your stoer view code match to country code 
+			$store = $this->storeFactory->create();
+			$store->load($countryCode, 'code');
+
+			// here we setting store code but this would be happenign for each request need to fix based on session logics so it must call only once		
+			$this->storeCookieManager->setStoreCookie($store);
+			$this->_storeManager->setCurrentStore($store);
+
+		// }
+
 		return null;
 	}
 }
